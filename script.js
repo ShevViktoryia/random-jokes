e7bad64534e0b410007a2dbc0c83eced896acde2
Victoria <shevvictoryia@gmail.com>
2024-08-24 15:27:40 +0300

    feat: add generate jokes in a btn

--- script.js
+++ script.js
@@ -2,5 +2,19 @@ async function getJokes() {
   const jokes = "./assets/data/jokes.json";
   const res = await fetch(jokes);
   const data = await res.json();
-  console.log(data);
+  return data;
 }
+
+async function getRandomJoke() {
+  const jokes = await getJokes("PromiseResult");
+  let random = Math.floor(Math.random() * (jokes.value.length - 1) + 1);
+  console.log(jokes.value[random].joke);
+  joke_text.textContent = jokes.value[random].joke;
+}
+
+const joke_text = document.querySelector(".joke");
+const btn = document.querySelector(".btn");
+
+btn.onclick = () => {
+  getRandomJoke();
+};
